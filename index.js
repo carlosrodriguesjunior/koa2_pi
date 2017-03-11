0663183c078f9adbbb02086bba8dbf4a1bda99ae
carlos rodrigues <carlosrodriguesjunior@gmail.com>
2017-03-11 19:23:45 -0300

    api powerOn e PowerOff

--- index.js
+++ index.js
@@ -1,28 +1,80 @@
 var Koa = require('koa');
 var Router = require('koa-router');
+var gpio = require('rpi-gpio');
+var sleep = require('sleep');
 
 var app = new Koa();
 var router = new Router();
+
+gpio.setup(18, gpio.DIR_OUT);
+gpio.setup(22, gpio.DIR_OUT);
 
 app
     .use(router.routes())
     .use(router.allowedMethods());
 
 
-router.get('/teste', async(ctx, next) => {
+router.get('/powerOn', async(ctx, next) => {    
 
-    ctx.body = await teste();
+    await powerOnLed(18);
+    sleep.sleep(2);
+    await powerOnLed(22);
+
+    ctx.status = 200;
+
+});
+
+router.get('/powerOff', async(ctx, next) => {
+
+    await powerOffLed(18);
+    sleep.sleep(2);
+    await powerOffLed(22);
+
+    ctx.status = 200;
 
 });
 
 
-function teste() {
+
+function powerOnLed(pin) {
 
     return new Promise((resolve, reject) => {
-        resolve('teste');
+
+        gpio.write(pin, true, (err)=>{
+
+            if (err) {
+                reject(err);
+                return;
+                }
+
+            resolve();
+
+        })
+        
     });
 
 
 };
+
+function powerOffLed(pin) {
+
+    return new Promise((resolve, reject) => {
+
+        gpio.write(pin, false, (err)=>{
+
+            if (err) {
+                reject(err);
+                return;
+                }
+
+            resolve();
+
+        })
+        
+    });
+
+
+};
+
 
 app.listen(3000);
